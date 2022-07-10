d497882ebba771929199b08bcbdd79f1e650d16b
shekhar-exp <99975096+shekhar-exp@users.noreply.github.com>
2022-07-10 16:19:11 +0530

    Added Proc File

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
